66631155dd6e56c09c3575f9ffd94b4061d4add7
tonnyvijayan <tonnyvijayan@gmail.com>
2021-04-28 22:29:31 +0530

    VideoCard

--- src/Components/VideoSection/VideoPage.jsx
+++ src/Components/VideoSection/VideoPage.jsx
@@ -5,6 +5,7 @@ import { useVideoManagement } from "../../Contexts/VideoContextProvider";
 import { Link } from "react-router-dom";
 import PlayListadd from "./Assets/playlist_add.svg";
 import Like from "./Assets/thumb_up_off.svg";
+import { useState } from "react";
 
 const opts = {
   height: "390",
@@ -15,6 +16,7 @@ const opts = {
 };
 
 export const VideoPage = () => {
+  const [show, setShow] = useState(false);
   const { state, dispatch } = useVideoManagement();
   const { videoId } = useParams();
   console.log({ videoId });
@@ -28,51 +30,89 @@ export const VideoPage = () => {
   const [requiredVideoId] = id;
 
   return (
-    <div class="video-page-video-section">
-      {newVideo.map((item) => {
-        return (
-          <div class="video-page-video-container">
-            <div class="video-page-thumbnail">
-              <Youtube
-                videoId={requiredVideoId}
-                opts={opts}
-                className="react-youtube-player"
-              />
-            </div>
-
-            <div class="video-page-video-bottom-section">
-              <a href="#" class="video-page-channel-icon">
-                <img
-                  src={item.channelImage}
-                  alt=""
-                  class="video-page-channel-icon-image"
+    <div>
+      <div class="video-page-video-section">
+        {newVideo.map((item) => {
+          return (
+            <div class="video-page-video-container">
+              <div class="video-page-thumbnail">
+                <Youtube
+                  videoId={requiredVideoId}
+                  opts={opts}
+                  className="react-youtube-player"
                 />
-              </a>
-              <div class="video-page-video-details">
-                <span class="video-page-video-title">{item.title}</span>
-                <span class="video-page-channel-name">{item.creator}</span>
-                <div class="video-page-video-view-data">
-                  <span class="video-page-views">{item.views}views</span> •
-                  <span class="video-page-date-posted">3daysAgo</span>
-                  {/* <>View</Link> */}
+              </div>
+
+              <div class="video-page-video-bottom-section">
+                <a href="#" class="video-page-channel-icon">
+                  <img
+                    src={item.channelImage}
+                    alt=""
+                    class="video-page-channel-icon-image"
+                  />
+                </a>
+                <div class="video-page-video-details">
+                  <span class="video-page-video-title">{item.title}</span>
+                  <span class="video-page-channel-name">{item.creator}</span>
+                  <div class="video-page-video-view-data">
+                    <span class="video-page-views">{item.views}views</span> •
+                    <span class="video-page-date-posted">3daysAgo</span>
+                    {/* <>View</Link> */}
+                  </div>
+                </div>
+                <div class="bottom-section-icons">
+                  <span
+                    class="icon-links"
+                    onClick={() => setShow((prev) => true)}
+                  >
+                    <img src={PlayListadd} alt="Playlist" />
+                  </span>
+                  <span class="icon-links">
+                    <img src={Like} alt="like" />
+                  </span>
                 </div>
               </div>
-              <div class="bottom-section-icons">
-                <span class="icon-links">
-                  <img src={PlayListadd} alt="Playlist" />
-                </span>
-                <span class="icon-links">
-                  <img src={Like} alt="like" />
-                </span>
-              </div>
             </div>
+          );
+        })}
+        {/* modal */}
+      </div>
+      {/* modal */}
+
+      <div
+        class={`modal-container ${show ? "show" : null}`}
+        id="modal-container"
+      >
+        <div class="modal">
+          <h2>PlayList</h2>
+          {/* <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            odit corporis minima quibusdam odio est voluptatem sapiente nisi
+            itaque doloribus voluptas a quae, fugiat temporibus aperiam. Ea
+            ipsam optio soluta.
+          </p> */}
+          <div>
+            <fieldset className="chekbox-container">
+              <input type="checkbox" name="playListCheckBox" />
+              <label htmlFor="playListCheckBox">PlayList1</label>
+            </fieldset>
+
+            <input
+              type="text"
+              placeholder="Create PlayList"
+              className="create-playList-input"
+            />
+            <button className="create-playList-button">+</button>
           </div>
-        );
-      })}
+          <button
+            class="button-primary-one"
+            id="close-modal"
+            onClick={() => setShow((prev) => false)}
+          >
+            Close
+          </button>
+        </div>
+      </div>
     </div>
-    // <div class="video-wrapper">
-    //   {/* hello */}
-    //   <Youtube videoId={requiredVideoId} opts={opts} class="youtube-player" />
-    // </div>
   );
 };
